f0800d0bd9b95bd6679bb89b82f3b5990b71b745
Maniyarasan <92170430+maniyadg@users.noreply.github.com>
2024-03-01 13:01:57 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -36,7 +36,7 @@ function Projects() {
               description="I crafted a dynamic MERN stack e-commerce platform with integrated chat functionality, delivering a seamless shopping experience.My portfolio proudly showcases a MERN stack online shopping application complete with real-time chat capabilities, offering users an immersive browsing and communication experience.In my latest project, I engineered a cutting-edge MERN stack e-commerce solution featuring a sophisticated chat system, elevating online shopping interactions to new levels of engagement and convenience."
               clientLink="https://github.com/maniyadg/instamart-frontend.git"
               serverLink="https://github.com/maniyadg/instamart-backend.git"
-              demoLink="https://friendly-narwhal-5df89d.netlify.app/category"
+              demoLink="https://friendly-narwhal-5df89d.netlify.app"
             />
           </Col>
 
